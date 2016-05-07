7cb68c4027044121004bd4604ba6c66bb42723f1
Julio Cesar <juliocesarrodriguezcruz@gmail.com>
2016-05-07 15:45:38 -0500

    Use ngRoute to load data from json file in the directive

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-app = angular.module("my-directive-test", []);
+app = angular.module("my-directive-test", ['ngRoute']);
 
 app.directive('myTest', [function () {
 	return {
 		restrict: 'E',
 		replace: false,
 		templateUrl: 'my-test.tpl.html',
-		controller: ["$scope", function ($scope) {
-			$scope.tests = [
-				{"name": "Test 1"},
-				{"name": "Test 2"}
-			]
+		controller: ['$scope', '$http', function ($scope, $http) {
+			$http.get('/my-test.data.json').success(function (data) {
+				$scope.tests = data.results
+			});
 		}]
 	}
 }]);
